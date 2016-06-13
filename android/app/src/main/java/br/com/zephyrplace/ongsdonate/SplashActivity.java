f920963e23b641c75c530dcbffeaaa67e995cb89
tmarques <tmarques@ciandt.com>
2016-06-13 16:50:04 -0300

    Initial commit

---
+++ android/app/src/main/java/br/com/zephyrplace/ongsdonate/SplashActivity.java
@@ -0,0 +1,17 @@
+package br.com.zephyrplace.ongsdonate;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
