6d745ad55fa68c8907698bb70cf58292ec445248
Fernanda Aline Vieira da Silva <fvieira@ciandt.com>
2016-08-02 19:32:58 -0300

    Adicionando as rotas de insert e update user

--- server/app/models/user.model.js
+++ server/app/models/user.model.js
@@ -2,6 +2,12 @@ var connection    = require('../connection.js');
 
 var userModel;
 
-exports.get = function(id, callback){
-  connection.query('SELECT * FROM user WHERE id = ' + id, callback);
+exports.get = function(id, callback) {
+  connection.query('SELECT * FROM users WHERE id = ' + id, callback);
+}
+exports.insert = function(params, callback) {
+  connection.query('INSERT INTO users (name, email, password) VALUES ("' + params.name + '","'+ params.email + '","' + params.password + '")', callback);
+}
+exports.update = function(params, callback) {
+  connection.query('UPDATE users SET name="' + params.name + '", email="'+ params.email + '", password="' + params.password + '" WHERE id=' + params.id, callback);
 }
