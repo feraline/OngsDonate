1dd7745d1cd08ead0caeda85b354607bfa77ed19
Regivaldo Silva <rferna73@its.jnj.com>
2016-08-04 13:04:01 -0300

    Adição de parametro no md5

--- server/app/utils/security.js
+++ server/app/utils/security.js
@@ -1,7 +1,7 @@
 var md5 = require('md5');
 
-exports.md5 = function(value){
+exports.md5 = function(value, timestamp){
   var date = new Date();
-  var precrypt = Number(date) + value;
+  var precrypt = (timestamp) ? Number(date) + value : value;
   return md5(precrypt);
 }
